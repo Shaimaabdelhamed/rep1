8e51ccd3ac010af3d2096b0e9b543bf4101ba2cc
ShaimaaAbdelhamed <shaimaa@yahoo.com>
2016-07-14 07:28:41 +0300

    version1

---
+++ WebApplication1/WebApplication1/WebForm1.aspx.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace requiredtasks
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            Timer1.Tick += Timer1_Tick;
+        }
+
+        private void Timer1_Tick(object sender, EventArgs e)
+        {
+            report rprt = new report();
+            using (StreamWriter w = File.AppendText(@"C:\Users\shaima\Source\Repos\rep1\WebApplication1\log.txt"))
+            {
+                rprt.Writetxt("Hi", w);
+
+            }
+        }
+    }
+}
